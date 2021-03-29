c8cf9b5c252498c0f5cd1944a0a56bd0208bda1c
rfournie <80350581+rfournie@users.noreply.github.com>
2021-03-29 12:02:27 +0200

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,8 @@
 import random
 def main():
-  roll = random.randint(1,6)
+dice_sum=0
+for i in range(0,dice_rolls):
+  roll = random.randint(1,6) dice_sum= dice_sum + roll
   print(f'You rolled a {roll}')
 
 
